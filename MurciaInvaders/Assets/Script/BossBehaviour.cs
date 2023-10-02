86f6f7e8d5b7b1a7ec24c3cd34c4e63dc35efbd8
jfuentesp <josefupe91@gmail.com>
2023-10-02 17:11:50 +0200

    Boss Movement
    - Boss behaviour moves as intended
    - Creating ScriptableObjects for different enemies

--- MurciaInvaders/Assets/Script/BossBehaviour.cs
+++ MurciaInvaders/Assets/Script/BossBehaviour.cs
@@ -15,7 +15,12 @@ public class NewBehaviourScript : MonoBehaviour
     private float m_SpawnTime;
     [SerializeField]
     //Movement speed of the boss to move in X Axis
-    private float m_BossSpeed = 3f;
+    private float m_BossSpeed = 1.4f;
+
+    [Header("Movement clamps")]
+    private float m_Clamp = 2f;
+
+    private Vector2 m_Direction;
 
     //Rigidbody of the boss Gameobject
     Rigidbody2D m_RigidBody;
@@ -28,7 +33,7 @@ public class NewBehaviourScript : MonoBehaviour
         //Loading components
         m_RigidBody = GetComponent<Rigidbody2D>();
         m_EnemyPool = GetComponent<Pool>();
-
+        m_Direction = new Vector2(1 * m_BossSpeed,0);
     }
 
     // Start is called before the first frame update
@@ -43,9 +48,25 @@ public class NewBehaviourScript : MonoBehaviour
         
     }
 
+    private void FixedUpdate()
+    {
+        float currentPosition = Mathf.Clamp(transform.position.x, -m_Clamp, m_Clamp);
+        Debug.Log("Current position boss: " + currentPosition);
+        BossMovement(currentPosition);
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         
+    }
+
+    private void BossMovement(float clampedPosition)
+    {
+        if (clampedPosition >= m_Clamp)
+            m_Direction.x = -1 * m_BossSpeed;
+        if (clampedPosition <= -m_Clamp)
+            m_Direction.x = 1 * m_BossSpeed;
+        m_RigidBody.velocity = m_Direction;
     }
 
 
---
+++ MurciaInvaders/Assets/ScriptableObjects/EnemyScriptableObject.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemyTypeStats", menuName = "Scriptable Objects/Enemy Type Stats")]
+public class Enemy : ScriptableObject
+{
+    [SerializeField]
+    private float m_EnemySpeed;
+    [SerializeField]
+    private int m_ScoreValue;
+    [SerializeField]
+    private Color m_EnemyColor;
+    [SerializeField]
+    private int m_MaxHitpoints;
+    [SerializeField]
+    private int m_Hitpoints;
+
+    //Accessors to get the value (similar to getters)
+    public float EnemySpeed => m_EnemySpeed;
+    public int ScoreValue => m_ScoreValue;
+    public Color Color => m_EnemyColor;
+    public int MaxHitpoints => m_MaxHitpoints;
+    public int Hitpoints => m_Hitpoints;
+
+    
+}
