fb22f091e333869f233fdadac44ba5e145593b1e
jfuentesp <josefupe91@gmail.com>
2023-11-01 04:01:21 +0100

    Remake of GameEvents
    - Remade Game Events to work properly. GUI Error was about GUI Script loading Instances on Start. It wasn't getting the Spawner instance correctly as it was slower (or order). Set the instances in UpdateGUI().
    
    - Set GameEventsVoid to GameEvents. Deleted and upgraded all those unnecessary GameEvents since GUI Changes.

--- StreetsOfRaval/Assets/GameEvents 1/Generic/Implementation/GameEventIntInt.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "GameEvent", menuName = "GameEvent/GameEvent - Integer Integer")]
-public class GameEventIntInt : GameEvent<int, int>
-{
-}
