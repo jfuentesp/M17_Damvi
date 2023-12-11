1315ee40a9c1ea02d74d99eca357836800423670
jfuentesp <josefupe91@gmail.com>
2023-12-11 19:00:42 +0100

    aaaaaaaaaaaaaaa

--- FPSFrenchPeopleShooter/Assets/Scripts/BehaviorScripts/MovableBehaviour.cs
+++ FPSFrenchPeopleShooter/Assets/Scripts/BehaviorScripts/MovableBehaviour.cs
@@ -1,18 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class MovableBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody m_Rigidbody;
+    [Header("Speed and rotation speed atributes")]
+    [SerializeField]
+    private float m_Speed;
+    [SerializeField]
+    private float m_RotationSpeed;
+
+    [Header("Limitations if it has a Y axis clamp")]
+    [SerializeField]
+    private float m_ClampY;
+
+    Vector3 m_Movement;
+    Vector3 m_RotationMovement;
+    Vector3 m_RotationMovementY;
+
+    private void Awake()
     {
-        
+        m_Rigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnMove(Vector3 direction)
     {
-        
+        m_Movement = direction;
+    }
+
+    public void OnRotate(float direction)
+    {
+        m_RotationMovementY = Vector3.up * direction;
+    }
+
+    private void Update()
+    {
+        //m_RotationMovementY = Vector3.up * Mathf.Clamp(m_RotationMovement.y, -m_ClampY, m_ClampY);
+        transform.Rotate(m_RotationMovementY * m_RotationSpeed * Time.deltaTime);
+        m_RotationMovementY = Vector3.zero;
+    }
+
+    private void FixedUpdate()
+    {
+        m_Rigidbody.velocity = m_Movement.normalized * m_Speed + Vector3.up * m_Rigidbody.velocity.y;
+        m_Movement = Vector3.zero;
     }
 }
---
+++ FPSFrenchPeopleShooter/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.EnhancedTouch;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(CapsuleCollider))]
+[RequireComponent(typeof(MovableBehaviour))]
+public class PlayerController : MonoBehaviour
+{
+    //Reference to the InputSystem
+    [Header("Reference to the Input System")]
+    [SerializeField]
+    private InputActionAsset m_InputAsset;
+    private InputActionAsset m_Input;
+    private InputAction m_MovementAction;
+    public InputAction MovementAction => m_MovementAction;
+    private InputAction m_CameraAction;
+    private InputActionMap m_CurrentActionMap;
+
+    //Setting up the rigid body variable
+    private Rigidbody m_Rigidbody;
+    private CapsuleCollider m_Collider;
+
+    //Declaration of components
+    private MovableBehaviour m_Moving;
+
+    private void Awake()
+    {
+        //Setting the Input Controls
+        Assert.IsNotNull(m_InputAsset);
+        m_Input = Instantiate(m_InputAsset);
+        m_CurrentActionMap = m_Input.FindActionMap("Character");
+        m_MovementAction = m_CurrentActionMap.FindAction("Movement");
+        m_CameraAction = m_CurrentActionMap.FindAction("RotationY");
+        m_Moving = GetComponent<MovableBehaviour>();
+    }
+
+    private void OnEnable()
+    {
+        //m_CurrentActionMap.FindAction("Attack1").performed += Shoot1;
+        //m_CurrentActionMap.FindAction("Attack2").performed += Shoot2;
+        //m_CurrentActionMap.FindAction("Jump").performed += Jump;
+        //m_CurrentActionMap.FindAction("Crouch").started += Crouch;
+        //m_CurrentActionMap.FindAction("Crouch").canceled += ReturnToIdleState;
+        //m_CurrentActionMap.FindAction("Interact").performed += Interact;
+        //m_CurrentActionMap.FindAction("Inventory").performed += Inventory;
+        m_CurrentActionMap.Enable();
+    }
+
+    private void OnDisable()
+    {
+        //m_CurrentActionMap.FindAction("Attack1").performed -= Shoot1;
+        //m_CurrentActionMap.FindAction("Attack2").performed -= Shoot2;
+        //m_CurrentActionMap.FindAction("Jump").performed -= Jump;
+        //m_CurrentActionMap.FindAction("Crouch").started -= Crouch;
+        //m_CurrentActionMap.FindAction("Crouch").canceled -= ReturnToIdleState;
+        //m_CurrentActionMap.FindAction("Interact").performed -= Interact;
+        //m_CurrentActionMap.FindAction("Inventory").performed -= Inventory;
+        m_CurrentActionMap.Disable();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(m_MovementAction.ReadValue<Vector3>() != Vector3.zero)
+        {
+            m_Moving.OnMove(m_MovementAction.ReadValue<Vector3>());
+        }
+        Debug.Log(m_CameraAction.ReadValue<float>());
+        if(m_CameraAction.ReadValue<float>() != 0)
+        {
+            m_Moving.OnRotate(m_CameraAction.ReadValue<float>());
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
